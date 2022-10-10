cc5262a89bb588ef58c2cdb60af412d4875a05b1
hwanginseong <his14721@gmail.com>
2022-10-10 15:32:08 +0900

    index.js 설명 추가

--- import,export.js
+++ import,export.js
@@ -30,10 +30,31 @@ export default {divide,multiple};
 //main.js 
 import math from "./math";
 
-/* 결론 : import 할 때 named export는 {} 을 사용해 이름을 명시해주고 default export는 그냥 새로이름을 지어준다.
+/* 
+결론 : import 할 때 named export는 {} 을 사용해 이름을 명시해주고 default export는 그냥 새로이름을 지어준다.
     즉, import conn , {getPath} 의 경우 conn은 default export를 하고 ,{getPath}는 named export를 한 모듈을 가져온것이다.
     이렇게 섞어서 사용 할수도 있다.
 */
+
+/**
+ * index.js
+ * 이를 사용할 경우 import문에서 파일경로 작성시 해당 폴더 작성만해줘도 자동으로 index.js파일을 import 한다.
+ * 
+ * Foo
+ *  ㄴ index.js
+ *  ㄴ app1.js 
+ *  ㄴ app2.js
+ *  ㄴ app3.js
+ * 
+ * 이러한 구조에서 Foo 디렉토리의 js들을 import해야 할때
+ * import Foo from 'Foo' 만 해주면 알아서 index.js를 import한다.
+ * 따라서 index.js에
+         import app1 from './app1.js'
+         import app2 from './app2.js'
+         import app3 from './app3.js'
+   와 같이  모듈들을 명시만 해주면 된다.
+ */
+
 
 /**
  * Dynamic Import
