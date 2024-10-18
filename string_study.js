b0853016dab180113ea1c5c4d4071485e842bd2b
DESKTOP-U5QQTJS\NANUM <his14721@gmail.com>
2024-10-18 11:13:58 +0900

    split 설명 추가함.

--- string_study.js
+++ string_study.js
@@ -108,3 +108,18 @@ if(replacedArr){
         Fitem.str = Fitem.str.replace(item,replaceStr);
     });
 }
+/*
+split() 함수는, 문자열을 'separator'로 잘라서, 'limit' 크기 이하의 배열에 잘라진 문자열을 저장하여 리턴합니다.
+separator (필수 아님)
+문자열을 잘라 줄 구분자 (문자열 또는 정규식)
+값이 입력되지 않으면 문자열 전체를 배열에 담아서 리턴합니다.
+
+limit (필수 아님)
+최대 분할 갯수
+*/
+const str = "apple banana orange";
+const arr = str.split(" ");
+document.writeln(arr.length); // 3
+document.writeln(arr[0]); // apple
+document.writeln(arr[1]); // banana
+document.writeln(arr[2]); // orange
